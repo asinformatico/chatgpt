8151de53a0881c1d40f1ab38110b7eb025a95833
@as_informatico <45702201+asinformatico@users.noreply.github.com>
2023-03-10 12:26:47 +0100

    Update chatgpt.py

--- chatgpt.py
+++ chatgpt.py
@@ -2,26 +2,36 @@ import openai
 import csv
 import sys
 import os
+import platform
 from colorama import Fore, Back, init
+
 init()
 
+nombre_sistema = platform.system()
+if nombre_sistema == "Windows":
+    os.system('cls')
+else:
+    os.system('clear')
+
 # En el archivo mi_api_key debe estar la Clave API que nos ha proporcionado OpenAI.
-os.system('clear')
 with open('mi_api_key', newline='') as key:
 	claves_api = csv.reader(key)
 	for clave in claves_api:
 		chars = '[\']'
 		clave = str(clave).translate(str.maketrans('', '', chars))
 		openai.api_key = clave
+		
 pregunta = ''
 salida = ['salir','exit','bye','adios', 'chao', 'quit']
-print(Back.RED + Fore.WHITE + '\n ChatGPT para TERMUX by @as_informatico \n\n')
+print(Back.RED + Fore.WHITE + '\nChatGPT para Línea de Comandos by @as_informatico\n\n')
 print(Back.BLACK + 'Uso:\n1- Escriba su consulta o petición y pulse Enter.\n2- Espere su respuesta.\n'
 	  '3- Para finalizar el programa puede utilizar:\nexit, salir, quit, adios, bye o chao.\n\n')
+
 while -1:
 	pregunta = input(Fore.WHITE + 'Introduzca su pregunta: ')
 	if pregunta in salida:
 		sys.exit(Fore.RED + 'Programa finalizado por el usuario')
+		
 	if pregunta.strip() == '':
 		print('Debe escribir algo antes de enviar...')
 	else:
